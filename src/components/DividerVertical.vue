ed4665c3a34f4f43a823b7a39450b4e7cbd627eb
AlienUrbano <paulogabrielneves@hotmail.com>
2025-05-12 23:26:21 -0300

    Refactor: Segregates the Divider components into 2 separate components with prop for size, and each with own orientation

---
+++ src/components/DividerVertical.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="flex justify-center items-center" :style="{ height: `${size}em` }">
+    <div class="w-[1px] h-full bg-[var(--separator-light)]"></div>
+  </div>
+</template>
+
+<script setup>
+const props = defineProps({
+  size: {
+    type: Number,
+    default: 4,
+  },
+})
+</script>
