203e35612c6060a5affa7c743b29e3b01a573bca
AlienUrbano <paulogabrielneves@hotmail.com>
2025-05-12 23:27:43 -0300

    feature:  Add NotesGrid component and create NoteArea  component

--- src/components/NotesGrid.vue
+++ src/components/NotesGrid.vue
@@ -1,9 +1,18 @@
 <template>
-    <div>
-
+  <div class="flex flex-row gap-2 justify-center items-center min-h-4/5">
+    <slot name="noteArea"></slot>
+    <div v-if="notesStore.getNotesLength() > 1" class="flex flex-col gap-4 justify-center items-center">
+        <DividerVertical :size=15 />
+        <button class="cursor-grabbing"><img src="@/assets/Icons/arrow-split.svg" alt="arrow-split" class="w-4 h-4" /></button>
+        <DividerVertical :size=15 />
     </div>
+    <slot v-if="notesStore.getNotesLength() > 1" name="noteArea"></slot>
+  </div>
 </template>
 
 <script setup>
+import DividerVertical from '@/components/DividerVertical.vue'
+import { useNotesStore } from '@/stores/notesStore'
 
+const notesStore = useNotesStore()
 </script>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,15 +1,22 @@
 <script setup>
-import AppContainer from '@/components/AppContainer.vue';
-import NavBar from '@/components/NavBar.vue';
-import Divider from '@/components/Divider.vue';
-import AppToolbar from '@/components/AppToolbar.vue';
+import AppContainer from '@/components/AppContainer.vue'
+import NavBar from '@/components/NavBar.vue'
+import Divider from '@/components/DividerHorizontal.vue'
+import AppToolbar from '@/components/AppToolbar.vue'
+import NotesGrid from '@/components/NotesGrid.vue'
+import NoteArea from '@/components/NoteCard/NoteArea.vue'
 </script>
 
-<template>  
-    <AppContainer> 
-      <NavBar />
-      <Divider />
-      <AppToolbar />
-      <Divider />
-    </AppContainer>
+<template>
+  <AppContainer>
+    <NavBar />
+    <Divider />
+    <AppToolbar />
+    <Divider />
+    <NotesGrid>
+      <template #noteArea>
+        <NoteArea />
+      </template>
+    </NotesGrid>
+  </AppContainer>
 </template>
